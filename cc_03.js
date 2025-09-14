82f6dc2f059107477ba92a4fa029b7422d905aaf
Isabella Jones <injones@usf.edu>
2025-09-14 00:08:02 -0400

    intial commit with JS and HTML

--- cc_03.js
+++ cc_03.js
@@ -1,4 +1,31 @@
 // Pens, Notebooks, Backpacks
 let customers = [
-    {name = Kenzie Ricer, email = }
-]
+    {name: "Kenzie Ricer", 
+    email: "kenzieR22@gmail.com",
+    purchases: [{pens: 10, notebooks: 4, backpacks: 1}] 
+}, {
+    name: "Nicole Turner",
+    email: "NTurner33@gmail.com",
+    purchases: [{pens: 20, notebooks: 6, backpacks: 2}]
+}, {
+    name: "Sabrina Carpenter", 
+    email: "SabrinaCar@gmail.com",
+    purchases: [{pens: 40, notebooks: 20, backpacks: 10}]
+}]
+
+customers.forEach(obj => console.log(
+    `Name: ${obj.name}`,
+    `| Email: ${obj.email}`,
+    `| Purchases: ${purchases}`
+));
+
+//customers.push({name: "Han Jisung", 
+    //email: "HanStrayKidz@gmail.com",
+    //purchases: [{pens: 60, notebooks: 5, backpacks: 1}]
+//})
+
+//my_shift = customers.shift()
+
+//customers.email[1] = "NicTurner@gmail.com"
+
+//customers.push(customer[0][2][1] = 6)
